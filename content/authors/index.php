1b753d2961d195596fcb098be922e1a6048f8907
Valery Votintsev <vvotintsev@gmail.com>
2012-09-12 13:21:05 +0300

    Fix titles for Vit profiles

--- content/authors/index.php
+++ content/authors/index.php
@@ -7,14 +7,18 @@
 	<div class="columntext">
           Виталий Невзоров
           <br>
-          <a href="https://forum.sources.ru/index.php?showuser=4453" title="Виталий Невзоров @ sources.ru" target="_blank"><img src="/img/Vit_src.jpg" alt="Виталий Невзоров" style="border: 1px solid rgb(221, 221, 221);"></a>
-          <a style="float:right;" href="https://forum.vingrad.ru/index.php?showuser=10" title="Виталий Невзоров @ vingrad.ru" target="_blank"><img src="/img/vit.jpg" alt="Виталий Невзоров" style="border: 1px solid rgb(221, 221, 221);"></a>
+          <a href="https://forum.sources.ru/index.php?showuser=4453" title="Виталий Невзоров на sources.ru" target="_blank"><img src="/img/Vit_src.jpg" alt="Виталий Невзоров" style="border: 1px solid rgb(221, 221, 221);"></a>
+          <a style="float:right;" href="https://forum.vingrad.ru/index.php?showuser=10" title="Виталий Невзоров на vingrad.ru" target="_blank"><img src="/img/vit.jpg" alt="Виталий Невзоров" style="border: 1px solid rgb(221, 221, 221);"></a>
 	</div>
       </div>
 
 
 
-<p>Создатель <strong>DRKB</strong> и&nbsp;долгое время единственный ее разработчик: <strong>Виталий Невзоров</strong> (<strong><a href="https://forum.vingrad.ru/index.php?showuser=10" title="Vit@vingrad" target="_blank">Vit</a></strong>, <strong><a href="https://forum.sources.ru/index.php?showuser=4453" title="Vit@sources" target="_blank">Vit</a></strong>).</p>
+<p>Создатель <strong>DRKB</strong> и&nbsp;долгое время единственный ее разработчик:
+<strong>Виталий Невзоров</strong>
+(<strong><a href="https://forum.vingrad.ru/index.php?showuser=10" title="Vit на сайте vingrad.ru" target="_blank">Vit</a></strong>,
+ <strong><a href="https://forum.sources.ru/index.php?showuser=4453" title="Vit на сайте sources.ru" target="_blank">Vit</a></strong>).
+</p>
 <p>Виталий является автором всех версий вплоть до&nbsp;<b>3.0</b> включительно. (Это последняя версия, выпущенная за его авторством.)</p>
 
 <p>К несчастью, Виталий Невзоров погиб в автокатастрофе 07 июля 2009г...</p>
@@ -29,6 +33,6 @@
 
 <p>Модульная сетка и&nbsp;начальный дизайн сайта разработан: <strong><a href="https://www.colorcat.ru/" title="Sofiyan (Caramel)" target="_blank">Sofiyan</a></strong> (<strong>Caramel</strong>).</p>
 
-<p>Онлайн-версия: <strong><a href="https://forum.sources.ru/index.php?showuser=2" title="Valery Votintsev (vot)" target="_blank">Valery Votintsev</strong> (vot)</a>.</p>
+<p>Онлайн-версия сайта: <strong><a href="https://forum.sources.ru/index.php?showuser=2" title="Valery Votintsev (vot)" target="_blank">Valery Votintsev</strong> (vot)</a>.</p>
 
 <p>Поддержка проекта: <a title="Исходники.ру" href="https://forum.sources.ru/" target="_blank">Исходники.Ру</a> и <a title="Vingrad Hosting" href="https://hosting.vingrad.ru/" target="_blank">Vingrad Hosting</a>.</p>
