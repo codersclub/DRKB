205946d2922430ed4af66b19679e9a4b25c0c30a
Valery Votintsev <vvotintsev@gmail.com>
2012-09-13 11:06:32 +0300

    Fix titles for Vit profiles

--- content/authors/index.php
+++ content/authors/index.php
@@ -1,6 +1,6 @@
 <h1>Авторы проекта</h1>
 
-      <div class="column" style="float:right;">
+      <div class="column" style="float:left; margin-right: 1em;">
 	<div class="columnbg">
           <div class="columnheader">Создатель DRKB</div>
         </div>
@@ -15,15 +15,17 @@
 
 
 <p>Создатель <strong>DRKB</strong> и&nbsp;долгое время единственный ее разработчик:
-<strong>Виталий Невзоров</strong>
-(<strong><a href="https://forum.vingrad.ru/index.php?showuser=10" title="Vit на сайте vingrad.ru" target="_blank">Vit</a></strong>,
- <strong><a href="https://forum.sources.ru/index.php?showuser=4453" title="Vit на сайте sources.ru" target="_blank">Vit</a></strong>).
+<strong>Виталий Невзоров</strong> (Vit):
+<br>
+&middot; <strong><a href="https://forum.vingrad.ru/index.php?showuser=10" title="Vit на сайте vingrad.ru" target="_blank">Vit на сайте vingrad.ru</a></strong>
+<br>
+&middot; <strong><a href="https://forum.sources.ru/index.php?showuser=4453" title="Vit на сайте sources.ru" target="_blank">Vit на сайте sources.ru</a></strong>
 </p>
 <p>Виталий является автором всех версий вплоть до&nbsp;<b>3.0</b> включительно. (Это последняя версия, выпущенная за его авторством.)</p>
 
 <p>К несчастью, Виталий Невзоров погиб в автокатастрофе 07 июля 2009г...</p>
 
-<p>&nbsp;</p>
+<div class="clear"></div>
 
 <p>Найти информацию об авторах статей и ответов можно по адресу:</p>
 <p>Vingrad.ru: <a href="https://forum.vingrad.ru/index.php?act=Members" target="_blank">https://forum.vingrad.ru/index.php?act=Members</a></p>
