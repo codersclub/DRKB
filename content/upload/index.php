3c0634c6d13506f04a61c52d0bd194ab242f8af0
Valery Votintsev <vvotintsev@gmail.com>
2012-09-28 18:14:54 +0300

    Move helper files to "lib" folder

--- content/upload/index.php
+++ content/upload/index.php
@@ -1,7 +1,7 @@
 <?php
 //	header('Content-type: text/html; charset=UTF-8');
 
-	include_once (_D_."/ninja.php");
+	include_once (_D_."/lib/ninja.php");
 
 	echo js_insert_ninja();
 	echo css_insert_ninja("224px");
