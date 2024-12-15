023350bc7eb90572d34443fc8a509e9566bafa53
Valery Votintsev <none@none>
2024-12-15 21:36:55 +0300

    Refactor tree js

--- _app/js/tree.js
+++ _app/js/tree.js
@@ -1,39 +1,69 @@
+function tree_open_all() {
+	$('.tree ul').toggleClass('all_off');
+}
+
+$(document).ready(function () {
+	
+	var url = window.parent.location.pathname;
+	var selector = 'a[href="'+url+'"]';
+	
+	$(selector).addClass('current').focus();
+	
+	var i = 6; // Max depth level
+	
+	while(url !== '/' && i) {
+		
+		var elem = document.getElementById(url);
+		
+		if(elem) {
+			elem.checked = true;
+		}
+		
+		url = url.replace(/\/$/, '').replace(/\/[^\/]*$/, '/');
+		
+		i--;
+	}
+	
+});
+
+
+
 // Add a Tree Expand/Collapse Handler
 var index = 1;
 
 var len = $('.tree li:has(ul)').length - 1;
 $.each($('.tree li:has(ul)'), function(itr) {
-  var tpl = `<input id="tree_${index}" type="checkbox">
-            <label for="tree_${index}"></label>`;
-  index++;
-  $(this).prepend(tpl);
-  if(itr == len){
-    currentNav();
-    expandToCurrent();
-  }
+	var tpl = `<input id="tree_${index}" type="checkbox">
+	<label for="tree_${index}"></label>`;
+	index++;
+	$(this).prepend(tpl);
+	if(itr == len){
+		currentNav();
+		expandToCurrent();
+	}
 });
 
 // Nav tree
 // TODO: Ajax tree?
 function currentNav(){
-  var currentURL = window.location.href;
-  currentURL = currentURL.slice(currentURL.indexOf('/', currentURL.indexOf('//') + 2)); //ToDo: simplify
-  if(currentURL.includes('?')){
-    currentURL = currentURL.slice(0, currentURL.indexOf('?'))
-  }
-  $('.tree a').each(function(){
-    if($(this).attr('href') == currentURL){
-      $(this).addClass('current');
-    }
-  });
-  // var elements = document.querySelectorAll('.tree li a');
-  //     Array.from(elements).forEach(function(element) {
-  //       if(element.getAttribute('href') == currentURL){
-  //         element.classList.add('current');
-  //       }
-  //     });
+	var currentURL = window.location.href;
+	currentURL = currentURL.slice(currentURL.indexOf('/', currentURL.indexOf('//') + 2)); //ToDo: simplify
+	if(currentURL.includes('?')){
+		currentURL = currentURL.slice(0, currentURL.indexOf('?'))
+	}
+	$('.tree a').each(function(){
+		if($(this).attr('href') == currentURL){
+			$(this).addClass('current');
+		}
+	});
+	// var elements = document.querySelectorAll('.tree li a');
+	//     Array.from(elements).forEach(function(element) {
+	//       if(element.getAttribute('href') == currentURL){
+	//         element.classList.add('current');
+	//       }
+	//     });
 }
 
 function expandToCurrent(){
-  $('.tree .current').parents('li').children('input[type=checkbox]').prop('checked', true);
+	$('.tree .current').parents('li').children('input[type=checkbox]').prop('checked', true);
 }
--- _app/tpl/tree_stop.php
+++ _app/tpl/tree_stop.php
@@ -4,37 +4,6 @@
 
 <script type="text/javascript" src="<?= APP ?>/js/jQuery.js"></script>
 
-<script>
-function tree_open_all() {
-    $('.tree ul').toggleClass('all_off');
-}
-
-$(document).ready(function () {
-    
-  var url = window.parent.location.pathname;
-  var selector = 'a[href="'+url+'"]';
-
-  $(selector).addClass('current').focus();
-
-  var i = 6; // Max depth level
-
-  while(url !== '/' && i) {
-
-    var elem = document.getElementById(url);
-
-    if(elem) {
-      elem.checked = true;
-    }
-
-    url = url.replace(/\/$/, '').replace(/\/[^\/]*$/, '/');
-
-    i--;
-  }
-
-});
-</script>
-
-
 <script src="<?= APP ?>/js/tree.js"></script>
 
 </body>
